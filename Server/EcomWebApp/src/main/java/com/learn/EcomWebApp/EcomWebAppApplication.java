df9fba2ca9ff11193d372b199fcc37c69d8abcd8
Sreshtha Mehrotra <sreshtha.mehrotra@gmail.com>
2024-09-08 16:25:56 +0530

    project setup

---
+++ Server/EcomWebApp/src/main/java/com/learn/EcomWebApp/EcomWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.learn.EcomWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomWebAppApplication.class, args);
+	}
+
+}
---
+++ Server/EcomWebApp/src/test/java/com/learn/EcomWebApp/EcomWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.EcomWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
